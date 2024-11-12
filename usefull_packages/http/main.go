912ced8bfaf9e82a1f57d5ed8a05da2d65ff4101
Leandro da Silva Junior <leandrojunior.dev@gmail.com>
2024-11-12 20:40:19 -0300

    http: implement headers and query params

--- usefull_packages/http/main.go
+++ usefull_packages/http/main.go
@@ -6,15 +6,28 @@ import (
 
 func main() {
 
-	http.HandleFunc("/", findZipCode)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", helloWord)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func findZipCode(w http.ResponseWriter, r *http.Request) {
+func helloWord(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	w.Write([]byte( "Hello, Word!"))
+
+	zipCode := r.URL.Query().Get( "zipcode")
+	if zipCode ==  "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := w.Write([]byte(zipCode))
+	if err != nil {
+		panic(err)
+	}
 }
 
